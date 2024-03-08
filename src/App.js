ef23c5f21393699c3cb2f49a5ebb11899190f49d
Ahmed Mustafa <Ahmedzakho925@gmail.com>
2024-03-08 22:05:48 +0300

    improve design and make responsive

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import FlashCards from "./FlashCards";
 export default function App() {
   return (
     <div className="App">
+      <h2>React JS FlashCards</h2>
+      <h4>click on the card to see the answer</h4>
       <FlashCards />
     </div>
   );
